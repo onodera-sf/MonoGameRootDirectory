70a5758128a70aa0032d9e1cf4c7c12ba9a6155e
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-05-22 09:28:35 +0900

    プロジェクト ファイルを追加します。

---
+++ RootDirectory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RootDirectory
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
